0a706514da47a512c5e847f481946baf314dce97
Emran Kia <emran.kia@gmail.com>
2024-06-17 16:25:53 +0200

    EKS Provider Simple

--- modules/alb/variable.tf
+++
@@ -1,7 +0,0 @@
-variable "alb_name" {}
-variable "alb_internal" {}
-variable "alb_load_balancer_type" {}
-variable "alb_security_groups" {}
-variable "alb_subnets" {}
-variable "alb_enable_deletion_protection" {}
-variable "alb_tags" {}
--- modules/lb_target_group/output.tf
+++
@@ -1,4 +0,0 @@
-output "tg_arn" {
-  description = "The ARN of the Target Group"
-  value       = aws_lb_target_group.web_tg.arn
-}
